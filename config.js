d1b27ef81ed76cfc24f4d14949c163ecf5710add
ahmedneem23 <40441103+ahmedneem23@users.noreply.github.com>
2018-06-21 03:39:47 +0200

    Add files via upload

---
+++ config.js
@@ -0,0 +1,5 @@
+exports.TOKEN = 'NDU5MDkyMDIxMDUxNzg1MjE2.DgxLOg.-fLFHNW4NLB9KLCUnJY2I8UzxNc'; // ya wr3 la tns5
+
+exports.PREFIX = 'x';
+
+exports.GOOGLE_API_KEY = 'AIzaSyAdORXg7UZUo7sePv97JyoDqtQVi3Ll0b8';
